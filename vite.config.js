f937af6ee08870853870839e29a8f4ee7e21c110
shreya <bshreya483@gamil.com>
2025-11-11 23:41:34 +0530

    Add deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/split-app/",
+
   plugins: [react()],
   
   // 👈 ADD THIS 'server' BLOCK
